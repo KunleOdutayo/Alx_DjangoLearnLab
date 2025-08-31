e526b9450878ac94e76454957822a32096ea8906
Kunle Odutayo <kunlay.odutayo@gmail.com>
2025-08-31 13:32:10 +0100

    Implementing & Interacting with Django Models

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField (max_length=200)
-    aur=thor = models.CharField (max_length=100)
+    aurthor = models.CharField (max_length=100)
     publication_year = models.IntegerField ()
 
     def __str__(self):
