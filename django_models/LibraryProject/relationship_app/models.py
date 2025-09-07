af06b39a36d3d280b0cff29cb137e0d9fc32c685
Kunle Odutayo <kunlay.odutayo@gmail.com>
2025-09-07 16:48:28 +0100

    Implementing Model Relationships in Django

--- django_models/LibraryProject/relationship_app/models.py
+++ django_models/LibraryProject/relationship_app/models.py
@@ -4,8 +4,8 @@ class Author(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
-        return __str__(self)
-    
+        return self.title
+     
 class Book(models.Model):
     title = models.CharField(max_length=200)
     Author = models.ForeignKey(Author, on_delete=models.CASCADE)
