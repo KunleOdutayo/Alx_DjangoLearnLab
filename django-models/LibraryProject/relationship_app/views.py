d6dca7414fa97289f1c253c45cb707123c8e3b5c
Kunle Odutayo <kunlay.odutayo@gmail.com>
2025-09-14 18:13:30 +0100

    Django Views amd URL Config

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -13,5 +13,5 @@ from .models import Book, Library
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/library_detail.html'
+    template_name = 'relationship_app/library_detail.html from .models import Library'
     context_object_name = 'library'
