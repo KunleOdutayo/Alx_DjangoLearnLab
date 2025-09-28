c47d270eed1771e73d3218cc38a16a7d43bafb1e
Kunle Odutayo <kunlay.odutayo@gmail.com>
2025-09-28 21:59:07 +0100

    Building Custom Views and Generic Views in Django REST Framework

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from django.contrib import admin
-from .views import BookListCreateAPIView, BookRetrieveUpdateDstroyAPIView
+from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
+BookListCreateAPIView, BookRetrieveUpdateDstroyAPIView
 
 urlpatterns = [
     path('books/',
@@ -14,4 +15,20 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls', namespace=rest_framework)),
+
+    path('books/', 
+        BookListView.as_view(),
+        name='book-list'),
+
+    path('book/create/',
+        BookCreateView.as_view(),
+        name='book-create'),
+    
+    path('book/<int:pk>/update/',
+        BookUpdateView.as_view(),
+        name='book-update'),
+         
+    path('books/<int:pk>/delete/',
+        BookDeleteView.as_view(),
+        name='book-delete'),
 ]
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, mixins
+from rest_framework.generics import GenericAPIView
 from .models import Book
 from .serializers import BookSerializer
 
@@ -16,3 +17,34 @@ class BookRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = BookSerializer
 
     permission_classes = [permissions.IsAuthenticated]
+
+class BookBaseView(GenericAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    permission_classes = [permissions.Is AuthenticatedOrReadOnly]
+
+class BookListView(mixins.ListModelMixin, BookBaseView):
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+class BookDetailView(mixins.RetrieveModelMixin, BookBaseView):
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+    
+class BookCreateView(mixins.CreateModelMixin, BookBaseView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    
+class BookUpdateView(mixins.UpdateModelMixin, BookBaseView):
+
+    def put(self, reuest, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+    
+class BookDeleteView(mixins.DestroyModelMixin, BookBaseView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def delete(self, request, *args, **kwargs):
+        return self.destry(request, *args, **kwargs)
